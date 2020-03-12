8533e39bfbc673a72b866f3e415417f736cc22d4
Ramesh <rameshnaidu.allu@gmail.com>
2020-03-12 13:52:14 +0530

    add user login & logout sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,23 @@
 class SessionsController < ApplicationController
+  
   def new
-    
+  end
+
+  def create
+    user = User.find_by(email:params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      flash[:success] = "Logged in Successfully!!!"
+      log_in user
+      remember user
+      redirect_to root_url
+    else
+      flash.now[:danger] = 'Invalid credentials'
+      render 'new'
+    end
+  end
+
+  def delete
+    log_out if logged_in?
+    redirect_to root_url
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,37 @@
 class User < ApplicationRecord
-  
+  attr_accessor :remember_token
+  before_save { self.email = email.downcase }
   before_create :create_remember_token
+  
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: {maximum: 100},
+                    format: {with: VALID_EMAIL_REGEX},
+                    uniqueness: {case_sensitive: false}
+  
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
   
-  def User.new_token
+  def self.new_token
     SecureRandom.urlsafe_base64
   end
   
-  def User.digest(token)
+  def self.digest(token)
     Digest::SHA1.hexdigest(token.to_s)
   end
-  
+ 
+  def remember
+    create_remember_token
+    save
+  end
+
+  def authenticated?(token)
+    token == remember_token
+  end
+
+  def forget
+    update_attribute(:remember_token, nil)
+  end
+
   private
     def create_remember_token
       self.remember_token = User.digest(User.new_token)
