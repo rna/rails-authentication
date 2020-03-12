ff1a668210e7ec90ffc43c020d005d73d1e615a3
Ramesh <rameshnaidu.allu@gmail.com>
2020-03-13 02:50:16 +0530

    add post model and create post views for new & index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   
+  resources :posts, only: [:new, :create, :index]
+  root 'posts#index'
+  
   get   '/login',  to: 'sessions#new'
   post  '/login',  to: 'sessions#create'
-  delete '/logout', to: 'sessions#delete'
+  delete '/logout', to: 'sessions#delete' 
 end
